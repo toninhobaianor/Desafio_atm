d50dbfc02af52ba47a86f838048a43d38b22356b
Antonio Alberto <85882536+toninhobaianor@users.noreply.github.com>
2024-06-26 14:30:20 -0300

    configurações iniciais

---
+++ config/expressConfig.ts
@@ -0,0 +1,19 @@
+import cors,{ CorsOptions } from "cors";
+import express ,{ Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+export const app: Express = express();
+
+const options: CorsOptions = {
+    credentials: true,
+    origin: process.env.APP_URL
+}
+
+app.use(cors(options));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}));
