f59af89cfba5ea7709257738c0e809324a4419b2
Antonio Alberto <85882536+toninhobaianor@users.noreply.github.com>
2024-07-24 13:27:00 -0300

    tentando lembrar

--- src/domains/caixa/service/CaixaService.ts
+++ src/domains/caixa/service/CaixaService.ts
@@ -1,24 +1,40 @@
 import { ValorEntrada,CedulasSaida } from "../model/CaixaModel";
 
-async function CriandoCedulas(Cedulas: CedulasSaida,chave: string,valor: Number) {
-    Cedulas.cedulas.set(chave,valor);
+async function CriandoCedulas(Cedulas: CedulasSaida,chave: number,valor: number) {
+    switch(chave){
+        case 1:
+            Cedulas.cedulas.set("100",valor);
+            break;
+        case 2:
+            Cedulas.cedulas.set("50",valor);
+            break;
+        case 3:
+            Cedulas.cedulas.set("20",valor);
+            break;
+        case 4:
+            Cedulas.cedulas.set("10",valor);
+            break;
+        case 5:
+            Cedulas.cedulas.set("5",valor);
+            break;
+        case 6:
+            Cedulas.cedulas.set("2",valor);
+            break;
+        default:
+            break;
+    }
 }
 
-async function CalcularCedulas(valor: ValorEntrada) {
+export async function CalcularCedulas(valor: ValorEntrada) {
     let val: number = valor.valor;
     let cedulas: CedulasSaida = {cedulas: new Map<string,Number>};
     var resto : number = 1;
-    while(resto != 0){
+    var div : number = 0;
+    const numbers = [100, 50, 20, 10, 5, 2];
+    while(div != 1){
 
-        if(val >= 100){
-            var div = val/100;
-            resto = val%100;
-            CriandoCedulas(cedulas,"100",div);
-            div = resto;
-        }
-        else if(val < 100 && val >= 80){
-            
-        }
     }
+    var i = 0;
     return cedulas;
 }
+
